f8870c9e75c83a624ed13472c8c1d585cc0f0946
Frédéric Maquin <maquin.frederic@gmail.com>
2015-02-28 15:02:54 +0100

    Add basic go app.

---
+++ gimmicode.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/codegangsta/martini-contrib/render"
+	"github.com/go-martini/martini"
+)
+
+func main() {
+	m := martini.Classic()
+	m.Use(render.Renderer())
+
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "index", nil)
+	})
+
+	m.Run()
+}
