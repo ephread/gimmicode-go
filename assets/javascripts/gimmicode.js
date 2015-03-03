b7d36a13d34dacc5afea33ae611b131b3ee2b0ea
Frédéric Maquin <maquin.frederic@gmail.com>
2015-03-03 20:12:34 +0100

    Skinjob. :sparkles:

--- assets/javascripts/gimmicode.js
+++ assets/javascripts/gimmicode.js
@@ -1,22 +1,35 @@
 // Nothing really fancy is this quick'n'dirty code.
+//
+// A delay is used the first time a request is made; that
+// way the HTML only be injected after the animation played.
 $(document).ready(function() {
 
   // Private vars.
+	var animationTimeoutId   = null;
+		
 	var lastEnteredCharacter = "";
 	var lock                 = false;
 	var receivedData         = null;
 	
+	var $inputArea           = $('.input-area');
+	var $unicode             = $('.unicode');
 	var $unicodeData         = $('.unicode-data');
 	var $characterInput      = $('.character-input');
 	
 	// Inject the received HTML data into the current page.
 	function showUnicode() {
+		$inputArea.removeClass('animate')
+
 		if (receivedData !== null) {
 			$unicodeData.html(receivedData);
+			$unicode.removeClass('spin')
 			
 			receivedData = null;
 			lock = false;
 		}
+		
+		clearTimeout(animationTimeoutId);
+		animationTimeoutId = null;
 	}
 	
 	// Send the inputed character and request the data.
@@ -29,12 +42,19 @@ $(document).ready(function() {
 	  if (shouldPost) {
 	    lock = true;
 
+			if (!$inputArea.hasClass('packed')) {
+				$inputArea.addClass('animate').addClass('packed');
+				animationTimeoutId = setTimeout(showUnicode, 1000);
+			}
+
+			$unicode.addClass('spin')
+
 	    $.get(
 	      $(location).attr('href') + 'unicode',
 	      {character: currentCharacter},
 	      function(data) {
 					receivedData = data;
-					showUnicode();
+					if (animationTimeoutId === null) showUnicode(data);
 					lastEnteredCharacter = currentCharacter;
 	      }
 	    )
